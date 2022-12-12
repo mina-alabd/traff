6026b5c575753165e7d6b685273751ad0b9f054f
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2022-12-12 22:30:08 +0300

    Update m.php

--- m.php
+++ m.php
@@ -8,7 +8,7 @@ $hijau = "\e[92m";
 $merah = "\e[91m";
 
 echo " ~~~~~~ ‌𝓐‌𝔂‌𝓶𝓪‌𝓷 ‌𝓔‌𝓰‌𝔂 ~~~~~\n  ";
-$url = 'https://falcon-plus.blogspot.com'; 
+$url = file_get_contents("https://pastebin.com/raw/X3NQe5C2"); 
 $max = "9999999999999999";
 
 error_reporting(0);
