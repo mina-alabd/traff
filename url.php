edf9518c8862d3afffbba811da9181b972f20c30
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2022-12-16 20:23:48 +0300

    Update url.php

--- url.php
+++ url.php
@@ -1,1 +1,19 @@
-https://falcon-plus.blogspot.com/2022/12/cups-2.5-oz.html
+<?php
+$biru = "\e[34m";
+$kuning = "\e[33m";
+$cyan = "\e[96m";
+$magenta = "\e[35m";
+$hijau = "\e[92m";
+$merah = "\e[91m";
+
+
+echo "$hijau ~~~~~~ AUTO  TRAFFIC  2020 ~~~~~\n  "; 
+$url ='https://falcon-plus.blogspot.com/2022/12/cups-2.5-oz.html';
+$url2 ='https://falcon-plus.blogspot.com/2022/11/clear-plastic-cover-garlic-portion-2-oz.html';
+$url3= 'https://falcon-plus.blogspot.com/2022/12/round-reed-pulp-container-with-plastic-cover-12-oz.html'; 
+$url4= 'https://falcon-plus.blogspot.com/2022/11/clear-plastic-cover-garlic-portion-1-oz.html'; 
+
+
+
+$max = "99999999998999";
+?> 
